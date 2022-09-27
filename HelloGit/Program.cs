755fbe9e4f6764d18ded31f1b8e4565dca707160
Biswajit637 <biswajitmishra637@gmail.com>
2022-09-27 15:57:17 +0530

    new dll added

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EMS;
 
 namespace HelloGit
 {
@@ -12,6 +13,8 @@ namespace HelloGit
         {
             Console.WriteLine("Hello !!");
             Console.WriteLine("Today is 27-09-2022");
+            Emp emp = new Emp { Id = 10, Name = "Bitu", Salary = 55000 };
+            Console.WriteLine($"{emp.Id} {emp.Name} {emp.Salary}");
 
         }
     }
